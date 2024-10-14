1c57a4ff5720faa8b2150ce303b4a53202143b4d
EnderMelech <ethanjudahw@gmail.com>
2024-10-14 10:59:49 -0400

    nicknames

--- TestFile.py
+++ TestFile.py
@@ -2,4 +2,10 @@ import datetime
 #print('Hello World!')
 todayDate = datetime.datetime.today()
 userName = input('What is your name? ')
+if userName.lower() == 'andrew gage':
+    userName = 'Hemmorgage'
+elif userName.lower() == 'ben abayev' or 'benjamin abayev':
+    userName = 'Denis(on) deez nuts'
+elif userName.lower() == 'elisha horowitz':
+    userName = 'Elish Navidad'
 print(f'Hello {userName}, today is {todayDate:%A, %B %d, %Y}!')
