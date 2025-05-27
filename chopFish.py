6423f3bf35b6938dd4fe218b113b5e207251ab01
EnderMelech <ethanjudahw@gmail.com>
2025-05-27 14:45:38 -0400

    even better now lesgo

--- chopFish.py
+++ chopFish.py
@@ -1,12 +1,26 @@
 # chopsticks stockfish
-hand=1
-# def hit(hand):
-#     hand=hand+1
-while hand<5:
-    move=input("your move: ")
-    if move=="hit":
-        # hit(hand)
-        hand=hand+1
-        print(hand)
-if hand>=5:
-    print("hand lost")
+leftHand=1
+rightHand=1
+hands=[leftHand,rightHand]
+while sum(hands)<10:
+    move=input("your move: hit left (hl), hit right (hr)\n")
+    if move=="hr":
+        if rightHand>=5:
+            print("nice try buddy, you already got rid of that")
+        elif rightHand<5:
+            rightHand=rightHand+1
+            if rightHand==5:
+                print("hand lost")
+    elif move=="hl":
+        if leftHand>=5:
+            print("nice try buddy, you already got rid of that")
+        elif leftHand<5:
+            leftHand=leftHand+1
+            if leftHand==5:
+                print("hand lost")
+    else:
+        print("try again")
+    hands=[leftHand,rightHand]
+    print(hands)
+if sum(hands)>=10:
+    print("game over")
