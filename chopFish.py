4b95d808a96d3973e9325bf644abb9530b492ce6
EnderMelech <ethanjudahw@gmail.com>
2025-05-27 15:40:07 -0400

    starting to make it a real game

--- chopFish.py
+++ chopFish.py
@@ -1,26 +1,30 @@
 # chopsticks stockfish
-leftHand=1
-rightHand=1
-hands=[leftHand,rightHand]
-while sum(hands)<10:
-    move=input("your move: hit left (hl), hit right (hr)\n")
-    if move=="hr":
-        if rightHand>=5:
-            print("nice try buddy, you already got rid of that")
-        elif rightHand<5:
-            rightHand=rightHand+1
-            if rightHand==5:
-                print("hand lost")
-    elif move=="hl":
-        if leftHand>=5:
-            print("nice try buddy, you already got rid of that")
-        elif leftHand<5:
-            leftHand=leftHand+1
-            if leftHand==5:
-                print("hand lost")
-    else:
-        print("try again")
-    hands=[leftHand,rightHand]
-    print(hands)
-if sum(hands)>=10:
-    print("game over")
+oneLeftHand=1
+twoLeftHand=1
+oneRightHand=1
+twoRightHand=1
+oneHands=[oneLeftHand,oneRightHand]
+twoHands=[twoLeftHand,twoRightHand]
+def oneTurn():
+    if sum(oneHands)<10:
+        move=input("Player One's move: hit left (hl), hit right (hr)\n")
+        if move=="hr":
+            if rightHand>=5:
+                print("nice try buddy, you already got rid of that")
+            elif rightHand<5:
+                rightHand=rightHand+1
+                if rightHand==5:
+                    print("hand lost")
+        elif move=="hl":
+            if leftHand>=5:
+                print("nice try buddy, you already got rid of that")
+            elif leftHand<5:
+                leftHand=leftHand+1
+                if leftHand==5:
+                    print("hand lost")
+        else:
+            print("try again")
+        hands=[leftHand,rightHand]
+        print(hands)
+    if sum(hands)>=10:
+        print("game over")
