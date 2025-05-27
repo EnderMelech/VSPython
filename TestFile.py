29d4162645e50d86048e99b6f7b6080a0e098003
EnderMelech <ethanjudahw@gmail.com>
2025-05-27 12:00:16 -0400

    making chopsticks stockfish, lists

--- TestFile.py
+++ TestFile.py
@@ -1,18 +1,34 @@
-print('Hello World!')
+# print('Hello World!')
 
-variable='Hello'
-print(f'{variable} World!')
+# variable='Hello'
+# print(f'{variable} World!')
 
-import datetime
-todayDate = datetime.datetime.today()
-userName = input('What is your name? ')
-print(f'Hello {userName}, today is {todayDate:%A, %B %d, %Y}!')
+# import datetime
+# todayDate = datetime.datetime.today()
+# userName = input('What is your name? ')
+# print(f'Hello {userName}, today is {todayDate:%A, %B %d, %Y}!')
 
-    # Open function to open the file "MyFile1.txt"
-    # (same directory) in append mode (able to write in it) and
-file1 = open("MyFile1.txt","a")
-    # store its reference in the variable file1
-    # and "MyFile2.txt" in D:\Text in file2 (w+ means you can read and write in it)
-file2 = open(r"D:\Text\MyFile2.txt","w+")
+#     # Open function to open the file "MyFile1.txt"
+#     # (same directory) in append mode (able to write in it) and
+# file1 = open("MyFile1.txt","a")
+#     # store its reference in the variable file1
+#     # and "MyFile2.txt" in D:\Text in file2 (w+ means you can read and write in it)
+# file2 = open(r"D:\Text\MyFile2.txt","w+")
 
-#File_object.read([n])
+# #File_object.read([n])
+
+# for i in range(0,13,3):
+#     print(i)
+
+L=[]
+print(L)
+L.append(6)
+print(L)
+L.append(9)
+print(L)
+M=[10,11,8,6,9]
+L.extend(M)
+print(L)
+for x in L:
+    print(x)
+print(str(L)[1:-1])
---
+++ chopFish.py
@@ -0,0 +1,11 @@
+# chopsticks stockfish
+hand=1
+if hand>=5:
+    print("hand lost")
+# def hit(hand):
+#     hand=hand+1
+move=input("your move: ")
+if move=="hit":
+    # hit(hand)
+    hand=hand+1
+    print(hand)
