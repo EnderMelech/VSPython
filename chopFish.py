3c2860caecc6a8470f4aaa2a3f694c2d545769f3
EnderMelech <ethanjudahw@gmail.com>
2025-09-15 16:11:00 -0400

    everything works now, made things more efficient

--- chopFish.py
+++ chopFish.py
@@ -1,95 +1,129 @@
 # chopsticks stockfish
-oneLeftHand=1
-twoLeftHand=1
-oneRightHand=1
-twoRightHand=1
-#oneHands=f'{str(oneLeftHand)}, {str(oneRightHand)}'
-#twoHands=f'{str(twoLeftHand)}, {str(twoRightHand)}'
-oneHands=[oneLeftHand,oneRightHand]
-twoHands=[twoLeftHand,twoRightHand]
-hands=f'Player One: {str(oneHands)} \nPlayer Two: {str(twoHands)}'
+oneHands = [1, 1]  # [left, right]
+twoHands = [1, 1]  # [left, right]
+
 def oneTurn():
-    global oneLeftHand
-    global twoLeftHand
-    global oneRightHand
-    global twoRightHand
-    global oneHands
-    global twoHands
-    global hands
-    move=input("Player One's move: hit left (hl), hit right (hr)\n")
-    if move=="hr":
-        if twoRightHand>=5:
+    global oneHands, twoHands
+    move = input("Player One's move: hit left (hl), hit right (hr)\n")
+    if move == "hr":
+        if twoHands[1] >= 5:
             print("nice try buddy, you already got rid of that")
+            return oneTurn()
         else:
-            move=input(f"Which hand will you use? Your left hand (lh): {oneLeftHand} or your right hand (rh): {oneRightHand}?")
-            if move=="lh":
-                twoRightHand=twoRightHand+oneLeftHand
-            elif move=="rh":
-                twoRightHand=twoRightHand+oneRightHand
+            hand = input(f"Which hand will you use? Your left hand (lh): {oneHands[0]} or your right hand (rh): {oneHands[1]}?\n")
+            if hand == "lh":
+                if oneHands[0] >= 5:
+                    print("you already lost that hand, go again")
+                    return oneTurn()
+                else:
+                    twoHands[1] += oneHands[0]
+            elif hand == "rh":
+                if oneHands[1] >= 5:
+                    print("you already lost that hand, go again")
+                    return oneTurn()
+                else:
+                    twoHands[1] += oneHands[1]
             else:
                 print("try again")
-            if twoRightHand==5:
+                return oneTurn()
+            if twoHands[1] >= 5:
                 print("hand lost")
-    elif move=="hl":
-        if twoLeftHand>=5:
+                twoHands[1] = 5
+    elif move == "hl":
+        if twoHands[0] >= 5:
             print("nice try buddy, you already got rid of that")
+            return oneTurn()
         else:
-            move=input(f"Which hand will you use? Your left hand (lh): {oneLeftHand} or your right hand (rh): {oneRightHand}?")
-            if move=="lh":
-                twoLeftHand=twoLeftHand+oneLeftHand
-            elif move=="rh":
-                twoLeftHand=twoLeftHand+oneRightHand
-            if twoLeftHand==5:
+            hand = input(f"Which hand will you use? Your left hand (lh): {oneHands[0]} or your right hand (rh): {oneHands[1]}?\n")
+            if hand == "lh":
+                if oneHands[0] >= 5:
+                    print("you already lost that hand, go again")
+                    return oneTurn()
+                else:
+                    twoHands[0] += oneHands[0]
+            elif hand == "rh":
+                if oneHands[1] >= 5:
+                    print("you already lost that hand, go again")
+                    return oneTurn()
+                else:
+                    twoHands[0] += oneHands[1]
+            else:
+                print("try again")
+                return oneTurn()
+            if twoHands[0] >= 5:
                 print("hand lost")
+                twoHands[0] = 5
     else:
         print("try again")
-    oneHands=[oneLeftHand,oneRightHand]
-    twoHands=[twoLeftHand,twoRightHand]
-    hands=f'Player One: {str(oneHands)} \nPlayer Two: {str(twoHands)}'
+        return oneTurn()
+
 def twoTurn():
-    global oneLeftHand
-    global twoLeftHand
-    global oneRightHand
-    global twoRightHand
-    global oneHands
-    global twoHands
-    global hands
-    move=input("Player Two's move: hit left (hl), hit right (hr)\n")
-    if move=="hr":
-        if oneRightHand>=5:
+    global oneHands, twoHands
+    move = input("Player Two's move: hit left (hl), hit right (hr)\n")
+    if move == "hr":
+        if oneHands[1] >= 5:
             print("nice try buddy, you already got rid of that")
+            return twoTurn()
         else:
-            move=input(f"Which hand will you use? Your left hand (lh): {twoLeftHand} or your right hand (rh): {twoRightHand}?")
-            if move=="lh":
-                oneRightHand=oneRightHand+twoLeftHand
-            elif move=="rh":
-                oneRightHand=oneRightHand+twoRightHand
+            hand = input(f"Which hand will you use? Your left hand (lh): {twoHands[0]} or your right hand (rh): {twoHands[1]}?\n")
+            if hand == "lh":
+                if twoHands[0] >= 5:
+                    print("you already lost that hand, go again")
+                    return twoTurn()
+                else:
+                    oneHands[1] += twoHands[0]
+            elif hand == "rh":
+                if twoHands[1] >= 5:
+                    print("you already lost that hand, go again")
+                    return twoTurn()
+                else:
+                    oneHands[1] += twoHands[1]
             else:
                 print("try again")
-            if oneRightHand==5:
+                return twoTurn()
+            if oneHands[1] >= 5:
                 print("hand lost")
-    elif move=="hl":
-        if oneLeftHand>=5:
+                oneHands[1] = 5
+    elif move == "hl":
+        if oneHands[0] >= 5:
             print("nice try buddy, you already got rid of that")
+            return twoTurn()
         else:
-            move=input(f"Which hand will you use? Your left hand (lh): {twoLeftHand} or your right hand (rh): {twoRightHand}?")
-            if move=="lh":
-                oneLeftHand=oneLeftHand+twoLeftHand
-            elif move=="rh":
-                oneLeftHand=oneLeftHand+twoRightHand
-            if oneLeftHand==5:
+            hand = input(f"Which hand will you use? Your left hand (lh): {twoHands[0]} or your right hand (rh): {twoHands[1]}?\n")
+            if hand == "lh":
+                if twoHands[0] >= 5:
+                    print("you already lost that hand, go again")
+                    return twoTurn()
+                else:
+                    oneHands[0] += twoHands[0]
+            elif hand == "rh":
+                if twoHands[1] >= 5:
+                    print("you already lost that hand, go again")
+                    return twoTurn()
+                else:
+                    oneHands[0] += twoHands[1]
+            else:
+                print("try again")
+                return twoTurn()
+            if oneHands[0] >= 5:
                 print("hand lost")
+                oneHands[0] = 5
     else:
         print("try again")
-    oneHands=[oneLeftHand,oneRightHand]
-    twoHands=[twoLeftHand,twoRightHand]
-    hands=f'Player One: {str(oneHands)} \nPlayer Two: {str(twoHands)}'
-while sum(oneHands)<10 and sum(twoHands)<10:
+        return twoTurn()
+
+def winConditionCheck():
+    if sum(oneHands) >= 10:
+        print("game over, player two won")
+        quit()
+    if sum(twoHands) >= 10:
+        print("game over, player one won")
+        quit()
+
+while sum(oneHands) < 10 and sum(twoHands) < 10:
+    print(f'Player One: {oneHands} \nPlayer Two: {twoHands}')
     oneTurn()
-    print(hands)
+    winConditionCheck()
+    print(f'Player One: {oneHands} \nPlayer Two: {twoHands}')
     twoTurn()
-    print(hands)
-if sum(oneHands)>=10:
-    print("game over")
-if sum(twoHands)>=10:
-    print("game over")
+    winConditionCheck()
