57d7ea692f73b3795c7b9772c696589822d8e7e1
EnderMelech <ethanjudahw@gmail.com>
2025-09-17 16:25:01 -0400

    started working on bot

--- chopFish.py
+++ chopFish.py
@@ -1,9 +1,22 @@
 # chopsticks stockfish
-oneHands = [1, 1]  # [left, right]
-twoHands = [1, 1]  # [left, right]
 
+# representation of both players' hands
+oneHands = [1, 1]  # [left, right]
+twoHands = [1, 1]
+botHands = [1, 1]
+position = str(oneHands+twoHands+botHands) # position is the current game position in one variable
+
+# bot instructions
+def botTurn(position):
+    # Open positions file
+    # Find line that contains the sum of the position
+    # Read a few characters further to get the line that is the beginning of the section of possible positions with that sum
+    # Find the current position in that section
+    # Read a few characters further to get the number that represents best move for the bot to make
+    sum(position)
+
+# function for player one's turn
 def oneTurn():
-    global oneHands, twoHands
     move = input("Player One's move: hit left (hl), hit right (hr)\n")
     if move == "hr":
         if twoHands[1] >= 5:
@@ -57,8 +70,8 @@ def oneTurn():
         print("try again")
         return oneTurn()
 
+# function for player two's turn
 def twoTurn():
-    global oneHands, twoHands
     move = input("Player Two's move: hit left (hl), hit right (hr)\n")
     if move == "hr":
         if oneHands[1] >= 5:
@@ -112,18 +125,48 @@ def twoTurn():
         print("try again")
         return twoTurn()
 
+# function to check if a player won
 def winConditionCheck():
+    if botOpp == True and sum(oneHands) >= 10 or sum(botHands) >= 10:
+        print("game over, bot won")
+        quit()
     if sum(oneHands) >= 10:
         print("game over, player two won")
         quit()
     if sum(twoHands) >= 10:
         print("game over, player one won")
         quit()
+    if sum(botHands) >= 10:
+        print("game over, player one won")
+        quit()
 
-while sum(oneHands) < 10 and sum(twoHands) < 10:
-    print(f'Player One: {oneHands} \nPlayer Two: {twoHands}')
-    oneTurn()
-    winConditionCheck()
-    print(f'Player One: {oneHands} \nPlayer Two: {twoHands}')
-    twoTurn()
-    winConditionCheck()
+# main game loop
+botOrNot = input("Would you like to play against a bot (bot) or another player (player)?\n").lower()
+if botOrNot == "bot":
+    botOpp = True
+    twoHands = None
+elif botOrNot == "player":
+    botOpp = False
+    botHands = None
+else:
+    print('invalid input, try again')
+    quit()
+
+if botOpp == True:
+    while sum(oneHands) < 10 and sum(botHands) < 10:
+        print(f'Player One: {oneHands} \nBot: {botHands}')
+        oneTurn()
+        winConditionCheck()
+        print(f'Player One: {oneHands} \nBot: {botHands}')
+        position = str(oneHands+botHands)
+        botTurn()
+        winConditionCheck()
+
+if botOpp == False:
+    while sum(oneHands) < 10 and sum(twoHands) < 10:
+        print(f'Player One: {oneHands} \nPlayer Two: {twoHands}')
+        oneTurn()
+        winConditionCheck()
+        print(f'Player One: {oneHands} \nPlayer Two: {twoHands}')
+        twoTurn()
+        winConditionCheck()
